8ecf1237cc5270048f1ed129a0cb0425385cc4da
deep121202 <123974120+deep121202@users.noreply.github.com>
2023-12-18 14:57:03 +0530

    Delete arm7 lpc21XX using keil software/blinkPort/blinkpin directory

--- arm7 lpc21XX using keil software/blinkPort/blinkpin/lib/adc/adc.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ADC_H_
-#define ADC_H_
-#include <stdint.h>
-
-void adc0_init(void);
-uint16_t adc0_read(unsigned char ch);
-
-#endif
--- arm7 lpc21XX using keil software/blinkPort/blinkpin/lib/spi/spi.h
+++
@@ -1,7 +0,0 @@
-#ifndef SPI_H_
-#define SPI_H_
-
-void spi_init (void);
-uint8_t spi_data(uint8_t cmd);
-
-#endif /* SPI_H_ */
